014edf848495af5721393885e6c515bebc27185a
Karsten <karsten@lilme.cc>
2025-03-27 10:37:04 +0800

    Update giscus-change-theme.js

--- assets/js/giscus-change-theme.js
+++ assets/js/giscus-change-theme.js
@@ -1,1 +1,14 @@
-function sendMessage(message) { const iframe = document.querySelector('iframe.giscus-frame'); if (!iframe) return; iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app'); }
+function changeGiscusTheme () {
+  let modeToggle = new ModeToggle();
+  const theme = modeToggle.mode === 'dark' ?  'dark' : 'light'
+  function sendMessage(message) {
+    const iframe = document.querySelector('iframe.giscus-frame');
+    if (!iframe) return;
+    iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
+  }
+  sendMessage({
+    setConfig: {
+      theme: theme
+    }
+  });
+}
